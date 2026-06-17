1c92b3634e2c682bc6047e4f1736a920012da591
MrAI-Rohan <rohanlalwani490@gmail.com>
2026-06-17 17:03:44 +0530

    Added func

--- utils.py
+++ utils.py
@@ -29,3 +29,16 @@ def compute_metrics(tp, fp, fn, tn):
         "fn": fn,
         "tn": tn
     }
+
+def convert_numerics(obj):
+        if isinstance(obj, dict):
+            return {k: convert_numerics(v) for k, v in obj.items()}
+        elif isinstance(obj, list):
+            return [convert_numerics(item) for item in obj]
+        elif isinstance(obj, str):
+            try:
+                return float(obj)
+            except ValueError:
+                return obj
+        else:
+            return obj
